52a78b95894a1f78e4f01dfb337d00230a10632a
lavaman131 <lavaalex3@gmail.com>
2023-01-05 13:08:43 -0800

    fixed contact page alignment

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,28 +1,41 @@
 const Contact = () => {
   return (
-    <div className="flex flex-col items-center justify-center bg-deep_space h-screen gap-6 md:gap-10">
-      <div className="flex flex-row gap-8">
-        <h1 className="font-baskerville font-bold text-creamy_white text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
-          Mail
-        </h1>
+    <div className="grid bg-deep_space">
+      <div className="flex flex-col mx-auto justify-center h-screen gap-6 md:gap-10">
+        <div className="flex flex-row gap-8">
+          <h1 className="font-baskerville font-bold text-creamy_white text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
+            Mail
+          </h1>
 
-        <a
-          className="font-baskerville text-sky-400 before:content-['_↗'] hover:text-sky-200 text-2xl md:text-3xl lg:text-4xl xl:text-5xl"
-          href="mailto:alavaee@bu.edu"
-        >
-          alavaee@bu.edu
-        </a>
-      </div>
-      <div className="flex flex-row gap-8">
-        <h1 className="font-baskerville font-bold text-creamy_white text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
-          Social Media
-        </h1>
-        <a
-          className="font-baskerville text-sky-400 before:content-['_↗'] hover:text-sky-200 text-2xl md:text-3xl lg:text-4xl xl:text-5xl"
-          href="https://www.linkedin.com/in/alexlavaee/"
-        >
-          linkedin
-        </a>
+          <a
+            className="font-baskerville text-sky-400 before:content-['_↗'] hover:text-sky-200 text-2xl md:text-3xl lg:text-4xl xl:text-5xl"
+            href="mailto:alavaee@bu.edu"
+          >
+            alavaee@bu.edu
+          </a>
+        </div>
+        <div className="flex flex-row gap-8">
+          <h1 className="font-baskerville font-bold text-creamy_white text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
+            Social Media
+          </h1>
+          <a
+            className="font-baskerville text-sky-400 before:content-['_↗'] hover:text-sky-200 text-2xl md:text-3xl lg:text-4xl xl:text-5xl"
+            href="https://www.linkedin.com/in/alexlavaee/"
+          >
+            LinkedIn
+          </a>
+        </div>
+        <div className="flex flex-row gap-8">
+          <h1 className="font-baskerville font-bold text-creamy_white text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
+            Developer
+          </h1>
+          <a
+            className="font-baskerville text-sky-400 before:content-['_↗'] hover:text-sky-200 text-2xl md:text-3xl lg:text-4xl xl:text-5xl"
+            href="https://github.com/lavaman131/"
+          >
+            GitHub
+          </a>
+        </div>
       </div>
     </div>
   );
