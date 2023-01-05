8321978401b7b60a45343b740f8b9dd3688cb69d
lavaman131 <lavaalex3@gmail.com>
2023-01-05 14:10:30 -0800

    added custom 404 page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./index.css";
 import Navbar from "./Navbar";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
@@ -16,7 +16,8 @@ function App() {
         <Route path="/About" element={<About />} />
         <Route path="/Projects" element={<Projects />} />
         <Route path="/Contact" element={<Contact />} />
-        <Route path="*" element={<Error />} />
+        <Route path="/404" element={<Error />} />
+        <Route path="*" element={<Navigate replace to="/404" />} />
       </Routes>
     </div>
   );
