f80d047913a23cace5226382e6a4dcddd4a06e06
lavaman131 <lavaalex3@gmail.com>
2023-01-05 14:46:45 -0800

    fix error page css

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -3,23 +3,25 @@ import corgi from "../imgs/corgi.jpg";
 
 const Error = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 bg-deep_space h-screen place-items-center gap-0">
-      <div className="flex flex-col items-center justify-center gap-10 m-4">
-        <h1 className="text-dark_gold font-bold font-baskerville text-6xl md:text-7xl lg:text-8xl xl:text-9xl">
-          Oops!
-        </h1>
-        <p className="text-center text-dark_gold font-baskerville text-lg md:text-2xl xl:text-3xl">
-          Sorry, an unexpected error has occurred.
-        </p>
-        <Link
-          className="text-dark_gold font-baskerville text-lg md:text-xl lg:text-2xl xl:text-3xl rounded-lg px-4 py-2 bg-magic_purple hover:bg-slate-700"
-          to="/"
-        >
-          Back Home
-        </Link>
-      </div>
-      <div className="flex flex-col items-center justify-center m-4">
-        <img alt="space corgi" src={corgi}></img>
+    <div className="h-screen bg-deep_space md:flex md:items-center md:justify-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 justify-center place-items-center gap-0">
+        <div className="flex flex-col items-center justify-center gap-10 m-4">
+          <h1 className="text-dark_gold font-bold font-baskerville text-6xl md:text-7xl lg:text-8xl xl:text-9xl">
+            Oops!
+          </h1>
+          <p className="text-center text-dark_gold font-baskerville text-lg md:text-2xl xl:text-3xl">
+            Sorry, an unexpected error has occurred.
+          </p>
+          <Link
+            className="text-dark_gold font-baskerville text-lg md:text-xl lg:text-2xl xl:text-3xl rounded-lg px-4 py-2 bg-magic_purple hover:bg-slate-700"
+            to="/"
+          >
+            Back Home
+          </Link>
+        </div>
+        <div className="flex flex-col items-center justify-center m-4">
+          <img className="rounded-xl h-2/3 w-2/3 md:w-screen" alt="space corgi" src={corgi}></img>
+        </div>
       </div>
     </div>
   );
