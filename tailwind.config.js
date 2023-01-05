ec995de47a5bdd0135fca505683badf2db3e295f
lavaman131 <lavaalex3@gmail.com>
2023-01-05 12:57:13 -0800

    fixed css settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: "media",
   theme: {
     extend: {
