9ff82b9050b47c99af5691f59144a902d8d55fc1
flora131 <nlavaee@umich.edu>
2025-11-12 09:37:05 -0800

    update to fix typos, add sources

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,11 +1,11 @@
 
 export default new Map([
 ["src/content/projects/cartoonify.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fcartoonify.mdx&astroContentModuleFlag=true")],
-["src/content/projects/ev-range-pred.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fev-range-pred.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/dinov2cpp.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fdinov2cpp.mdx&astroContentModuleFlag=true")],
+["src/content/projects/graphic-memoir-analysis.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fgraphic-memoir-analysis.mdx&astroContentModuleFlag=true")],
+["src/content/projects/ev-range-pred.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fev-range-pred.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/grammar-ninja.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fgrammar-ninja.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/hand-gesture-recognition.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fhand-gesture-recognition.mdx&astroContentModuleFlag=true")],
-["src/content/projects/graphic-memoir-analysis.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fgraphic-memoir-analysis.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/human-connectome.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fhuman-connectome.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/motion-gpt1.5.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fmotion-gpt1.5.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/object-tracking.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fobject-tracking.mdx&astroContentModuleFlag=true")],
